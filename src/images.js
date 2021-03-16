c1bab4ff46a62c9d5e5cfd0eb9e8dfb36e242b4a
Gustavo Silva <gustavohfts1@gmail.com>
2021-03-15 22:01:42 -0300

    Try to fix image issue

--- src/images.js
+++ src/images.js
@@ -1,11 +1,11 @@
-import manto from "url:./assets/manto.jpg";
+import manto_cover from "url:./assets/manto_cover.jpg";
 import manto1 from "url:./assets/manto1.jpg";
 import manto2 from "url:./assets/manto2.jpg";
 import manto3 from "url:./assets/manto3.jpg";
 
 // TODO: fix this once PR lands
 export default {
-  "manto.jpg": manto,
+  "manto_cover.jpg": manto_cover,
   "manto1.jpg": manto1,
   "manto2.jpg": manto2,
   "manto3.jpg": manto3,
